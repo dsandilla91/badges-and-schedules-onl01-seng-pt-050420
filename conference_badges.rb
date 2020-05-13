4f5db275c223a3e068828d980a9db91634220cf0
Deborah Santillan <s.debb@yahoo.com>
2020-05-13 01:02:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,13 @@ def assign_rooms(attendees)
   end 
 end
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+   batch_badge_creator(attendees).each  do |sentence|
+     puts sentence
+   end 
+   assign_rooms(attendees).each do |sentence|
+     puts sentence
 end 
-
+end 
   
   
   
